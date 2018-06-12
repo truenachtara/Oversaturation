0e6bfe9cbbe214b7d3946fd5009ac0980b172b26
Nachie <david.shema00@gmail.com>
2018-06-11 23:09:22 -0500

    first commit

---
+++ main/java/com/nachie/oversaturation/FoodStats.java
@@ -0,0 +1,16 @@
+package com.nachie.oversaturation;
+
+public class FoodStats extends net.minecraft.util.FoodStats {
+
+    @Override
+    public void addStats(int foodLevelIn, float foodSaturationModifier)
+    {
+        int extraSaturation = foodLevelIn - (20 - this.foodLevel);
+        if (extraSaturation > 0){
+            foodLevelIn = 20 - this.foodLevel;
+            foodSaturationModifier += (float) extraSaturation;
+        }
+        this.foodLevel = foodLevelIn + this.foodLevel;
+        this.foodSaturationLevel = this.foodSaturationLevel + (float)foodLevelIn * foodSaturationModifier * 2.0F;
+    }
+}
